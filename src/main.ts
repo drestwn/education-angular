18de6ac387beec04ea3615377007fe01bb22d25a
drestwn <andremakmursetaiwan@gmail.com>
2023-07-04 21:31:02 +0700

    feat:add home component & feature

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 /*
 *  Protractor support is deprecated in Angular.
 *  Protractor is used in this example for compatibility with Angular documentation tools.
+*  is where the app start running
 */
 import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
